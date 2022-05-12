f61806a8dc42c38fa243580c5ac3e3de562e3c45
hugosenari <hugosenari@gmail.com>
2022-05-12 11:51:41 -0300

    chore: re index fmod

--- flake.nix
+++ flake.nix
@@ -7,6 +7,30 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
+  inputs."fmod-master".dir   = "master";
+  inputs."fmod-master".owner = "nim-nix-pkgs";
+  inputs."fmod-master".ref   = "master";
+  inputs."fmod-master".repo  = "fmod";
+  inputs."fmod-master".type  = "github";
+  inputs."fmod-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."fmod-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."fmod-v0_1_0".dir   = "v0_1_0";
+  inputs."fmod-v0_1_0".owner = "nim-nix-pkgs";
+  inputs."fmod-v0_1_0".ref   = "master";
+  inputs."fmod-v0_1_0".repo  = "fmod";
+  inputs."fmod-v0_1_0".type  = "github";
+  inputs."fmod-v0_1_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."fmod-v0_1_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."fmod-v0_1_1".dir   = "v0_1_1";
+  inputs."fmod-v0_1_1".owner = "nim-nix-pkgs";
+  inputs."fmod-v0_1_1".ref   = "master";
+  inputs."fmod-v0_1_1".repo  = "fmod";
+  inputs."fmod-v0_1_1".type  = "github";
+  inputs."fmod-v0_1_1".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."fmod-v0_1_1".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
